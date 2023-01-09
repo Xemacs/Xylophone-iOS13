713de3fd704b2b125f2fc9a533773da11c4f9807
james stark <james.starkg@gmail.com>
2023-01-09 22:34:48 +1100

    Finish project

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cKeyButton: UIButton!
+    
+    var player =  AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+//        print(sender.currentTitle);
+        playSound(keyName: sender.currentTitle!)
+        cKeyButton.alpha = 1;
+    }
+    
+    @IBAction func cKeyPressed(_ sender: UIButton) {
+        cKeyButton.alpha = 0.4;
+    }
+    
+    func playSound(keyName: String) {
+        let soundUrl = Bundle.main.path(forResource: keyName, ofType: "wav")
+        do {
+            player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundUrl!))
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
-
